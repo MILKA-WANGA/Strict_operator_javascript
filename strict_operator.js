e00ddd4a64e5aa346bbbf59a6803b6e9a7afc2cf
MILKA-WANGA <83470011+MILKA-WANGA@users.noreply.github.com>
2023-04-11 08:14:14 -0700

    Initial Commit

---
+++ strict_operator.js
@@ -0,0 +1,20 @@
+function strictOperator()
+{
+    if(3=='3')
+    {
+    return "It is true";
+}
+return "False";
+}
+console.log(strictOperator())
+
+function strictOperators()
+{
+    if(3==='3')
+    {
+    return "It is true";
+}
+return "False";
+}
+console.log(strictOperators())
+    
